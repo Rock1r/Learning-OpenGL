3787d34b10d7b9b4fb73c8cf3d9ad1ec41ef865b
Rock1r <2449brv@gmail.com>
2024-01-03 22:01:41 +0200

    Added ImGui and ClearColor test

--- src/App.cpp
+++ src/App.cpp
@@ -17,26 +17,31 @@
 #include "glm\glm.hpp"
 #include "glm\gtc\matrix_transform.hpp"
 
+#include "imgui\imgui.h"
+#include "imgui\imgui_impl_glfw.h"
+#include "imgui\imgui_impl_opengl3.h"
+#include "tests\TestClearColor.h"
+
+
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    window = glfwCreateWindow(1920, 1000, "Hello World", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     GLCall(glfwSwapInterval(1));
@@ -45,82 +50,42 @@ int main(void)
     {
         std::cout << "Error" << "\n";
     }
-    int MaxTextureImageUnits;
-    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &MaxTextureImageUnits);
-    std::cout << "Slots : " << MaxTextureImageUnits << std::endl;
-    std::cout << glGetString(GL_VERSION) << std::endl;
     {
-        float positions[] = 
-        { 
-            -0.5f, -0.5f, 0.0f, 0.0f, 
-            0.5f, -0.5f, 1.0f, 0.0f, 
-            0.5f, 0.5f, 1.0f, 1.0f, 
-            -0.5f, 0.5f, 0.0f, 1.0f 
-        };
-
-        unsigned int indices[] = { 0, 1, 2, 2, 3, 0 };
-
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-        
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-        
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-        
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(-1.0f, 1.0f, -1.2f, 1.2f, -1.0f, 1.0f);
-
-        Shader shader("res/shaders/basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 3.9f, 0.1f, 5.3f, 2.3f);
-        shader.SetUniformMat4f("u_MVP", proj);
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
+    
         Renderer renderer;
+        
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 440");
 
-        float red = 0.0f;
-        float increment = 0.05f;
-        /* Loop until the user closes the window */
+        test::TestClearColor test;
+
+        std::cout << glGetString(GL_VERSION) << std::endl;
+
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
-
+            
             renderer.Clear();
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", red, 0.1f, 5.3f, 2.3f);
+            test.OnUpdate(0.0f);
+            test.OnRender();
 
-            Texture texture("res/textures/deathscreenblack.png");
-            texture.Bind();
-            shader.SetUniform1i("u_Texture", 0);
-
-            renderer.Draw(va, ib, shader);
-            
-            if (red > 1.0f) 
-            {
-                increment = -0.05f;
-            }
-            else if (red < 0.0f) 
-            {
-                increment = 0.05f;
-            }
-            red += increment;
-            /* swap front and back buffers */
+            ImGui_ImplGlfw_NewFrame();
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui::NewFrame();
+            test.OnImGuiRender();
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
             glfwSwapBuffers(window);
 
-            /* Poll for and process events */
             glfwPollEvents();
         }
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
